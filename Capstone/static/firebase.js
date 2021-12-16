f4dd243a846348379409d893d2a401486a114f4b
Karthikeya77 <75258359+Karthikeya77@users.noreply.github.com>
2021-12-16 15:27:44 +0530

    Final changes final part

--- Capstone/static/firebase.js
+++ Capstone/static/firebase.js
@@ -1,6 +1,4 @@
-
-
-  // TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
